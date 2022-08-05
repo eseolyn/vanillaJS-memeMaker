948e9a95959a17bcd23dce730adf2f5f5d90d16d
eunseolYun <skopdt73@gmail.com>
2022-08-05 13:07:15 +0900

    canvasAPI

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-alert("hello")
+const canvas = document.querySelector("canvas")
+// context = brush
+const ctx = canvas.getContext("2d")
+// show js canvas's size
+canvas.width = 800;
+canvas.height = 800;
